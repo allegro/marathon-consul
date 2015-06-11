084bbc57e1c3089896494fc3ca92bc98868e61af
Brian Hicks <brian@brianthicks.com>
2015-06-11 13:57:54 -0700

    apps: remove old test

--- apps/app_test.go
+++ apps/app_test.go
@@ -3,7 +3,6 @@ package apps
 import (
 	"encoding/json"
 	"github.com/stretchr/testify/assert"
-	"strings"
 	"testing"
 )
 
@@ -71,38 +70,4 @@ func TestAppKV(t *testing.T) {
 	kv := testApp.KV()
 	assert.Equal(t, kv.Key, testApp.Key())
 	assert.Equal(t, kv.Value, jsonified)
-}
-
-func ParseApps(event []byte) (apps []*App, err error) {
-	if strings.Index(string(event), "api_post_event") != -1 {
-		container := APIPostEvent{}
-		err = json.Unmarshal(event, &container)
-		if err != nil {
-			return nil, err
-		}
-
-		apps = []*App{&container.App}
-	} else if strings.Index(string(event), "deployment_info") != -1 {
-		container := DeploymentInfoEvent{}
-		err = json.Unmarshal(event, &container)
-		if err != nil {
-			return nil, err
-		}
-
-		apps = container.Plan.Target.Apps
-	} else if strings.Index(string(event), "app_terminated_event") != -1 {
-		container := AppTerminatedEvent{}
-		err = json.Unmarshal(event, &container)
-		if err != nil {
-			return nil, err
-		}
-
-		apps = []*App{&App{ID: container.AppID}}
-	}
-
-	if len(apps) == 0 {
-		err = ErrNoApps
-	}
-
-	return apps, err
 }
