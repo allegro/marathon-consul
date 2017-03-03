22ca1571a4f9c9ce40260d5a4574e7754f605ca3
Mateusz Gajewski <mateusz.gajewski@allegrogroup.com>
2017-03-03 12:41:52 +0100

    fix type

--- consul/consul_test.go
+++ consul/consul_test.go
@@ -251,7 +251,7 @@ func TestGetServicesUsingProviderWithRetriesOnAgentFailure_ShouldRetryConfigured
 	consul.config.AgentFailuresTolerance = 2
 	consul.config.RequestRetries = 4
 
-	var called uint32
+	var called uint64
 	provider := func(agent *consulapi.Client) ([]*service.Service, error) {
 		called++
 		return nil, fmt.Errorf("error")
