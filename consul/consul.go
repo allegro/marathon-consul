5920d947044f766727edf8d8c430812ffb53c0d5
Mark Selby <tokenrain@users.noreply.github.com>
2017-05-29 07:55:43 -0400

    Set health check path to root (/) if empty (#236)
    Fixes a regression introduced in 65c47ca78377.
    When healthchekc has not defined path in Marathon
    then we should map it to root path (/).
    
    Fixes: #235

--- consul/consul.go
+++ consul/consul.go
@@ -328,7 +328,11 @@ func (c *Consul) marathonToConsulChecks(task *apps.Task, healthChecks []apps.Hea
 
 		switch check.Protocol {
 		case "HTTP", "HTTPS", "MESOS_HTTP", "MESOS_HTTPS":
-			if parsedURL, err := url.ParseRequestURI(check.Path); err == nil {
+			path := "/"
+			if check.Path != "" {
+				path = check.Path
+			}
+			if parsedURL, err := url.ParseRequestURI(path); err == nil {
 				if check.Protocol == "HTTP" || check.Protocol == "MESOS_HTTP" {
 					parsedURL.Scheme = "http"
 				} else {
@@ -341,7 +345,7 @@ func (c *Consul) marathonToConsulChecks(task *apps.Task, healthChecks []apps.Hea
 				log.WithError(err).
 					WithField("Id", task.AppID.String()).
 					WithField("Address", serviceAddress).
-					Warnf("Could not parse provided path: %s", check.Path)
+					Warnf("Could not parse provided path: %s", path)
 			}
 		case "TCP", "MESOS_TCP":
 			consulCheck.TCP = fmt.Sprintf("%s:%d", serviceAddress, port)
