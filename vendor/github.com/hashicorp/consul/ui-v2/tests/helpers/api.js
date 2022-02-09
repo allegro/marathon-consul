b95d95f1481b1bf6c07910a77612a1e8d649bb71
Tomasz Ziarko <tomasz.ziarko@allegro.pl>
2022-02-09 15:18:52 +0100

    migrate to go modules

--- vendor/github.com/hashicorp/consul/ui-v2/tests/helpers/api.js
+++
@@ -1,4 +0,0 @@
-import getAPI from '@hashicorp/ember-cli-api-double';
-import setCookies from 'consul-ui/tests/helpers/set-cookies';
-import typeToURL from 'consul-ui/tests/helpers/type-to-url';
-export default getAPI('/consul-api-double', setCookies, typeToURL);
--- vendor/github.com/hashicorp/consul/ui-v2/tests/pages/settings.js
+++
@@ -1,6 +0,0 @@
-import { create, visitable, clickable } from 'ember-cli-page-object';
-
-export default create({
-  visit: visitable('/settings'),
-  submit: clickable('[type=submit]'),
-});
